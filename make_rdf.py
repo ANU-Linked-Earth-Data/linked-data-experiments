cbc0a5bb6aae863d7bee908fa25a22de0ff358e5
Sam Toyer <sam@qxcv.net>
2016-03-24 10:40:04 +1100

    Use OWLified CovJSON

--- make_rdf.py
+++ make_rdf.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
-from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from argparse import ArgumentParser, FileType
 from collections import namedtuple
-from json import load
+from datetime import datetime
+from json import load, dumps, JSONEncoder
+from pytz import timezone
+from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from urllib.parse import quote_plus
 
 LDE = Namespace('http://example.com/lde#')
 GEO = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+LGDDS = Namespace('http://www.example.com/linkedGDDS#')
+OWL = Namespace('http://www.w3.org/2002/07/owl#')
 
 Accident = namedtuple('Accident', ['locations', 'uri', 'message', 'datetime'])
 Location = namedtuple('Location', ['lat', 'lon', 'street', 'suburb', 'uri'])
+
+
+def parse_date(date_str):
+    # Parse string (no timezone)
+    rest = date_str[:-4]
+    time_format = '%Y-%m-%d%H:%M:%S'
+    dt = datetime.strptime(rest, time_format)
+
+    # Now get right timezone
+    tz = timezone('Australia/Canberra')
+    assert date_str[-4:] in {'AEDT', 'AEST'}
+    # We don't even have to handle the AEDT/AEST case because pytz is smart
+    # enough to do it for us
+    dt = tz.localize(dt)
+
+    return dt
 
 
 def accident_data(tweet):
@@ -27,7 +47,8 @@ def accident_data(tweet):
             lat=lat, lon=lon, street=street, suburb=suburb, uri=loc_uri
         ))
     return Accident(
-        locations=locs, uri=uri, message=tweet['normalised'], datetime=tweet['datetime']
+        locations=locs, uri=uri, message=tweet['normalised'],
+        datetime=parse_date(tweet['datetime'])
     )
 
 
@@ -53,14 +74,66 @@ def accident_triples(tweet, data, ident):
         rv.append((loc_uri, LDE.streetName, Literal(loc.street)))
         rv.append((loc_uri, LDE.suburbName, Literal(loc.suburb)))
         rv.append((loc_uri, GEO.lat, Literal(loc.lat)))
-        rv.append((loc_uri, GEO['long'], Literal(loc.lon))) # heh
+        rv.append((loc_uri, GEO['long'], Literal(loc.lon)))  # heh
     return rv
 
 
+class DatetimeJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+
+def accident_coverage_triples(txy_list, accident_urls):
+    assert len(txy_list) == len(accident_urls)
+    axis = LDE.AccidentAxis
+    cov = LDE.AccidentDataCoverage
+    dom = LDE.AccidentDataDomain
+    rng = LDE.AccidentDataRange
+
+    # Boilerplate for coverage and domain
+    yield from [
+        (cov, RDF.type, OWL.NamedIndividual),
+        (cov, RDF.type, LGDDS.Coverage),
+        (cov, LGDDS.hasDomain, dom),
+        (cov, LGDDS.hasRange, rng),
+        (dom, RDF.type, OWL.NamedIndividual),
+        (dom, RDF.type, LGDDS.txyTrajectoryDomain),
+        (cov, LGDDS.hasAxis, axis)
+    ]
+
+    # Domain for the accidents
+    json_txy = dumps(txy_list, cls=DatetimeJSONEncoder)
+    json_components = dumps(['t', 'x', 'y'])
+    yield from [
+        (axis, RDF.type, OWL.NamedIndividual),
+        (axis, RDF.type, LGDDS.txyTupleAxis),
+        (axis, LGDDS.axisSize, Literal(len(txy_list))),
+        (axis, LGDDS.axisValues, Literal(json_txy)),
+        (axis, LGDDS.tupleComponents, Literal(json_components))
+    ]
+
+    # Range for the accident
+    json_urls = dumps(accident_urls)
+    yield from [
+        (rng, RDF.type, OWL.NamedIndividual),
+        (rng, RDF.type, LGDDS.Range),
+        (rng, LGDDS.hasDataType, LGDDS.StringDataType),
+        (rng, LGDDS.rangeValues, Literal(json_urls))
+    ]
+
+
 def build_graph(tweets):
-    # TODO: Should also build coverage according to CovJSON spec here.
     rv = Graph()
     rv.namespace_manager.bind('lde', LDE)
+    rv.namespace_manager.bind('linkedgdds', LGDDS)
+    rv.namespace_manager.bind('owl', OWL)
+
+    # These two lists will be used to construct the coverage
+    txy_list = []
+    accident_url_list = []
+
     for tweet in slow(tweets, 'tweets processed so far', total=len(tweets)):
         data = accident_data(tweet)
         if data is None:
@@ -69,8 +142,18 @@ def build_graph(tweets):
         # First add data describing the accident
         for triple in accident_triples(tweet, data, ident):
             rv.add(triple)
-        # Now, for each location mentioned, add the accident to our coverage at
-        # that location
+
+        # Build up lists necessary to produce coverage
+        t = data.datetime
+        for loc in data.locations:
+            x = loc.lon
+            y = loc.lat
+            txy_list.append((t, x, y))
+            accident_url_list.append(ident)
+
+    for triple in accident_coverage_triples(txy_list, accident_url_list):
+        rv.add(triple)
+
     return rv
 
 parser = ArgumentParser()
